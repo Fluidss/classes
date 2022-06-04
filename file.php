4cdc3c145a5109197f292c060d4405fc4570aa07
minusamix <49316188+Fluidss@users.noreply.github.com>
2022-06-04 10:59:27 +0300

    Create file.php

---
+++ file.php
@@ -0,0 +1,26 @@
+class File
+{
+    public $path;
+    public function __construct(string $path)
+    {
+        if (file_exists($path)) {
+
+            $this->path = __DIR__ . '/' . $path;
+        } else {
+            echo 'неправильный путь к файлу';
+        }
+    }
+    public function read(): string
+    {
+        $str = file_get_contents($this->path);
+        return $str;
+    }
+    public function write(string $text)
+    {
+        file_put_contents($this->path, $text, FILE_APPEND);
+    }
+    public function create($path)
+    {
+        file_put_contents($path, null);
+    }
+}
